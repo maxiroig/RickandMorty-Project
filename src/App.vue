99ba6a5372889dfc4ded749a8169471a4d815912
Maxi Roig <128360089+maxiroig@users.noreply.github.com>
2023-06-21 18:31:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="w-screen h-screen flex items-center justify-center bg-gray-800 text-white">
+    <h3 class="text-4xl">Here is the starting point</h3>
+  </div>
+</template>
+
+<style scoped>
+</style>
